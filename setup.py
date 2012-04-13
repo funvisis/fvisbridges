6c00df2965a680202a5596d8e3790ff8e2c8e6e1
Daniel Ampuero <danielmaxx@gmail.com>
2012-04-13 18:52:04 +0000

    Se separa la aplicación bridges de fvisgeo.
    --
    danielmaxx

---
+++ setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+    
+'''
+IMPORTANTE: los campos que se describen a continuación deben ser rellenados en su totalidad.
+Aquellos campos que NO SE VAYAN A UTILIZAR, se deben BORRAR.
+'''
+
+
+setup(name='fvisbridges',
+      version='',
+      author='',
+      author_email='',
+      url='',
+      download_url='',
+      description='',
+      long_description='',
+      packages=['fvisbridges'],
+      py_modules=[''],
+      provides=[''],
+      keywords='',
+      license='',
+      classifiers=['Development Status :: Alpha',
+                   'Intended Audience :: Developers',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 2.6+',
+                   'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+                   'License :: OSI Approved :: GNU Affero General Public License v3',
+                   'Topic :: Internet',
+                   'Topic :: Scientific/Engineering :: GIS',
+                  ],
+       requires=[''],
+     )
